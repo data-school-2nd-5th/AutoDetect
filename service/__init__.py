53fe135570f69ab7d0147ebe203f64b21956334f
Minseong Kim <23519641+Binghagoon@users.noreply.github.com>
2026-04-07 10:43:29 +0900

    feat: delete greeting

--- service/__init__.py
+++ service/__init__.py
@@ -2,5 +2,4 @@
 
 from .cwe_orchestrator import CweSyncOrchestrator
 from .cwe_runtime import build_orchestrator
-from .greeting import get_greeting
 from .save_files import upload_by_targz_body, ls
