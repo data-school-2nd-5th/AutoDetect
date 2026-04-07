9883f83eb6ac6e19fd9bcbdc32ce17bd14b5923d
Minseong Kim <23519641+Binghagoon@users.noreply.github.com>
2026-04-07 10:44:57 +0900

    fix: remove none import

--- function_app.py
+++ function_app.py
@@ -1,10 +1,8 @@
 import azure.functions as func
-from blueprint.v1 import bp as v1_bp
 from blueprint.cwe import bp as cwe_bp
 from blueprint.monitor import bp as monitor_bp
 
 app = func.FunctionApp(http_auth_level=func.AuthLevel.ANONYMOUS)
 
-app.register_blueprint(v1_bp)
 app.register_blueprint(cwe_bp)
 app.register_blueprint(monitor_bp)
