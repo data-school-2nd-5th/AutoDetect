43188dfb0b77878ee211ce1723fecc21fd76205f
Minseong Kim <23519641+Binghagoon@users.noreply.github.com>
2026-04-15 17:00:57 +0900

    feat: add test code for ASCII

---
+++ tests/base64_example.py
@@ -0,0 +1,18 @@
+﻿from base64 import b64encode, b64decode
+import sys
+
+if hasattr(sys.stdout, "reconfigure"):
+    sys.stdout.reconfigure(encoding="utf-8")
+
+text = "Hello! 안녕 World! こんにちは"
+encoded = b64encode(text.encode("utf-8")).decode("utf-8")
+print(f"{encoded=}")
+
+decoded = b64decode(encoded).decode("utf-8")
+print(f"{decoded=}")
+
+code_snippet = dbutils.widgets.get("code_snippet")
+
+code_snippet = dbutils.widgets.get("code_snippet")
+code_snippet = b64decode(code_snippet).decode("utf-8")
+# print(f"{code_snippet=}")
